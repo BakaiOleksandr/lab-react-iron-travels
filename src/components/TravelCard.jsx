495e764241428ee7aeef2844983bdc161b27906d
Oleksandr Bakai <saniabakay@gmail.com>
2025-08-18 19:40:52 +0200

    iteration4

---
+++ src/components/TravelCard.jsx
@@ -0,0 +1,28 @@
+function TravelCard({plan, deleteItem}) {
+  return (
+    <div key={plan.id} className="item-container">
+      <div className="image-container">
+        <img src={plan.image} />
+      </div>
+      <div className="item-description">
+        <h3>
+          {plan.destination} ({plan.days} Days)
+        </h3>
+        <p className="description">{plan.description}</p>
+        <p className="price">
+          Price: <span>{plan.parts[0].cost} €</span>
+        </p>
+        <div className="labels">
+          {plan.parts[0].cost >= 1500 && <p>Premium</p>}
+          {plan.allInclusive && <p>All-Inclusive</p>}
+        </div>
+        {plan.parts[0].cost <= 350 && <p className="great-deal">Great Deal</p>}
+      </div>
+      <button className="delete-btn" onClick={() => deleteItem(plan.id)}>
+        Delete
+      </button>
+    </div>
+  );
+}
+
+export default TravelCard;
--- src/components/TravelList.jsx
+++ src/components/TravelList.jsx
@@ -1,5 +1,6 @@
 import travelPlansData from '../assets/travel-plans.json';
 import {useState} from 'react';
+import TravelCard from './TravelCard';
 
 function TravelList() {
   const [items, setItems] = useState(travelPlansData);
@@ -13,38 +14,10 @@ function TravelList() {
 
   return (
     <div className="items-container">
-      {items.map((item) => {
-        return (
-          <div key={item.id} className="item-container">
-            <div className="image-container">
-              <img src={item.image} />
-            </div>
-            <div className="item-description">
-              <h3>
-                {item.destination} ({item.days} Days)
-              </h3>
-              <p className="description">{item.description}</p>
-              <p className="price">
-                Price: <span>{item.parts[0].cost} €</span>
-              </p>
-              <div className='labels'>
-                {item.parts[0].cost >= 1500 && (
-                  <p >Premium</p>
-                )}
-                {item.allInclusive && (
-                  <p>All-Inclusive</p>
-                )}
-              </div>
-              {item.parts[0].cost <= 350 && (
-                <p className="great-deal">Great Deal</p>
-              )}
-            </div>
-            <button className='delete-btn' onClick={()=>deleteItem(item.id)}>Delete</button>
-          </div>
-        );
-      })}
+      {items.map((item) => (
+        <TravelCard key={item.id} plan={item} deleteItem={deleteItem} />
+      ))}
     </div>
   );
 }
-
 export default TravelList;
