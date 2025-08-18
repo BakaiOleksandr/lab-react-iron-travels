dbce8e31387d0b8af3a554b0a65f08408a1250fc
Oleksandr Bakai <saniabakay@gmail.com>
2025-08-18 19:08:36 +0200

    delete-btn

--- src/components/TravelList.jsx
+++ src/components/TravelList.jsx
@@ -4,32 +4,42 @@ import {useState} from 'react';
 function TravelList() {
   const [items, setItems] = useState(travelPlansData);
 
+  const deleteItem = (itemId) => {
+    const filteredItems = items.filter((item) => {
+      return item.id !== itemId;
+    });
+    setItems(filteredItems);
+  };
+
   return (
     <div className="items-container">
       {items.map((item) => {
         return (
-          <div className="item-container">
+          <div key={item.id} className="item-container">
             <div className="image-container">
-              <img src={item.image}></img>
+              <img src={item.image} />
             </div>
             <div className="item-description">
               <h3>
                 {item.destination} ({item.days} Days)
               </h3>
-              <p className='description'>{item.description}</p>
-              <p className='price'>
+              <p className="description">{item.description}</p>
+              <p className="price">
                 Price: <span>{item.parts[0].cost} €</span>
               </p>
-              <div className="">
+              <div className='labels'>
                 {item.parts[0].cost >= 1500 && (
-                  <button className="premium-inclusive">Premium</button>
+                  <p >Premium</p>
                 )}
                 {item.allInclusive && (
-                  <button className="premium-inclusive">All-Inclusive</button>
+                  <p>All-Inclusive</p>
                 )}
               </div>
-              {item.parts[0].cost<=350&&<button className='great-deal'>Great Deal</button>}
+              {item.parts[0].cost <= 350 && (
+                <p className="great-deal">Great Deal</p>
+              )}
             </div>
+            <button className='delete-btn' onClick={()=>deleteItem(item.id)}>Delete</button>
           </div>
         );
       })}
